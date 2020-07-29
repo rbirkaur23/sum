4002671533ffa6c5d0f81658bf535d7516e44862
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-29 22:54:20 +0530

    Created sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n1,n2,result;
+  cout<<"Enter two numbers: ";
+  cin>>n1>>n2;
+  result=n1+n2;
+  cout<<"Sum of "<<n1<<" and "<<n2<<" = "<<result;
+  return 0;
+}
